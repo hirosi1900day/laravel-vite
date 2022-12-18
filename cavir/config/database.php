78d107f972e423d8174c2dcd365ffe3cb96cc661
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2022-12-18 23:45:47 +0900

    database周りsqliteに変更

--- cavir/config/database.php
+++ cavir/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
@@ -46,10 +46,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
--- cavir/vite.config.js
+++ cavir/vite.config.js
@@ -8,12 +8,6 @@ export default defineConfig({
             input: 'resources/js/app.js',
             refresh: true,
         }),
-        server({
-            host: true,
-            hmr: {
-                host: 'localhost',
-            },
-        }),
         vue({
             template: {
                 transformAssetUrls: {
